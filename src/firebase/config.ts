3b822d52ca5a55d0c9faa10356b99ff95e9be4ef
Shreesha Venkatram <shreeshavenkatram99@gmail.com>
2026-01-30 11:20:23 +0000

    export const firebaseConfig = {   apiKey: "AIzaSyBKyhG5uHwWl23-6T6ueiYiG

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-544175764-b8623",
-  "appId": "1:261401370625:web:0a840806e11c944efe2000",
-  "apiKey": "AIzaSyBKyhG5uHwWl23-6T6ueiYiGBhiSzTbJPA",
-  "authDomain": "studio-544175764-b8623.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "261401370625"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
